bd1cd1f00a23e43b21dac9dc0a8b37820abf729c
rayjonesjay <rayjaymuiruri@gmail.com>
2024-07-30 22:10:32 +0300

    main.go

--- main.go
+++ main.go
@@ -1,9 +1,32 @@
 package main
 
 import (
+	// "JIFUNZE/backend"
 	"JIFUNZE/backend"
+	"JIFUNZE/blockchain"
+	"fmt"
 )
 
 func main() {
+	// Simulate adding a course completion
+	blockchain.AddCourseCompletionToBlockchain("user1", "basic-math")
+	blockchain.AddCourseCompletionToBlockchain("user2", "science")
+
+	blockchain.AddCourseCompletionToBlockchain("user3", "social")
+	// Load the blockchain to verify the data
+	bc, err := blockchain.LoadBlockchain("blockchain.json")
+	if err != nil {
+		fmt.Println("Error loading blockchain:", err)
+		return
+	}
+
+	// Print the loaded blockchain for verification
+	for _, block := range bc.Blocks {
+		fmt.Printf("Block %d:\n", block.Index)
+		fmt.Printf("  Timestamp: %d\n", block.Timestamp)
+		fmt.Printf("  Transactions: %v\n", block.Transactions)
+		fmt.Printf("  PrevHash: %s\n", block.PrevHash)
+		fmt.Printf("  Hash: %s\n", block.Hash)
+	}
 	backend.StartServer()
 }
