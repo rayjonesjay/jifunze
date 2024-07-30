dbf860d6e03f9e08a8acca0f9c71bd9f4822dff5
rayjonesjay <rayjaymuiruri@gmail.com>
2024-07-30 14:59:13 +0300

    deleted some files

--- backend/models/course.go
+++
@@ -1,8 +0,0 @@
-package models
-
-type Course struct {
-    ID          int    `json:"id"`
-    Title       string `json:"title"`
-    Description string `json:"description"`
-    Language    string `json:"language"`
-}
